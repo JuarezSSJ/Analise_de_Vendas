18c13621a73ebdf117e8806387376ef2b4b5a63b
Juarez Santana <juarezsantanajunior@gmail.com>
2025-04-22 11:17:34 -0300

    total de vendas em um determinado período

--- analise_vendas.py
+++ analise_vendas.py
@@ -12,3 +12,18 @@ base["Data"] = pd.to_datetime(base["Data"])
 base["Ano-Mes"] = base["Data"].dt.to_period("M")
 
 base.dtypes
+
+#Qual foi o total de vendas em um determinado período?
+
+#Setembro
+vendas_set_24 = base[["Ano-Mes", "Produto", "Quantidade", "Preço Unitário", "Valor Total"]]
+vendas_set_24 = vendas_set_24.groupby(["Ano-Mes", "Produto"])[["Quantidade", "Preço Unitário", "Valor Total"]].sum().reset_index()
+vendas_set_24 = vendas_set_24[(vendas_set_24["Ano-Mes"] == "2024-09")].sort_values(by = "Valor Total", ascending=False)
+vendas_set_24
+
+#Produto que foi mais vendido
+#vendas_set_24.info()
+prod_mais_vend_set_24 = vendas_set_24.iloc[0]
+prod_mais_vend_set_24
+
+#Produto que teve um maior valor total no acumulado do mês: Notbook
