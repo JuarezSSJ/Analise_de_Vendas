2449a681a571395ef7584bf456c47317e48c1b5f
Juarez Santana <juarezsantanajunior@gmail.com>
2025-04-22 11:11:22 -0300

    Convertendo coluna em Data

--- analise_vendas.py
+++ analise_vendas.py
@@ -3,4 +3,12 @@ import pandas as pd
 #importando base
 base = pd.read_csv("vendas.csv")
 
-base
+base.head()
+
+#convertendo coluna Data em datatime
+
+base["Data"] = pd.to_datetime(base["Data"])
+
+base["Ano-Mes"] = base["Data"].dt.to_period("M")
+
+base.dtypes
